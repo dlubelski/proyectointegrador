e563800d024d2327b9ae09dc691c7ff457627392
mrubaja@udesa.edu.ar <mrubaja@udesa.edu.arc:/Users/Usuario/Desktop/Programacin/proyecto>
2022-06-10 10:17:21 -0300

    forms

---
+++ js/forms.js
@@ -0,0 +1,14 @@
+let forms = document.querySelector('form');
+let buscar = document.querySelector('.buscar');
+forms.addEventListener('submit',function(evento){
+    evento.preventDefault();
+    console.log('no se manda el form');
+    if(buscar.value == ''){
+        alert('Este campo es obligatorio')
+    }else if(buscar.value.length <=3){
+        alert('Este campo debe tener mas de tres caracteres')
+    }else{
+        this.submit()
+    }
+})
+console.log()
