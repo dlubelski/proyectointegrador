6725cf930a171eb03b159ed4377645f6c319787c
serenagraciano <sgraciano@udesa.edu.ar>
2022-06-10 17:25:18 -0300

    ayuda index

--- js/albums.js
+++ js/albums.js
@@ -1,21 +1,22 @@
-let urlTracks = "https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/tracks"
+let urlAlbums = "https://api.allorigins.win/raw?url=https://api.deezer.com/album/302127"
 
-fetch(urlTracks)
+fetch(urlAlbums)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
         console.log(data);
         let info=data.data
-        let section = document.querySelector(".song")
+        let section = document.querySelector(".album")
         let article="";
         for (let i = 0; i < 5; i++) {
             article += `<article class="item">
-        <p>${info[i].title}</p>
+        <p>${tracks.data[i]}</p>
 </article>`
         }
         console.log(article);
         section.innerHTML += article
+        
     })
     .catch(function (error) {
         console.log(error);
