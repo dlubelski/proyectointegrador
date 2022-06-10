37e952ac8c81b560f89c0e2d1adb00541bbc4e15
serenagraciano <sgraciano@udesa.edu.ar>
2022-06-10 11:06:14 -0300

    tracks primera parte

--- js/index.js
+++ js/index.js
@@ -1,20 +1,23 @@
-let url = "https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/albums"
+let urlTracks = "https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/tracks"
 
-fetch(url)
+fetch(urlTracks)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
         console.log(data);
-        let info = data.data
+        let info = tracks.data
         let section = document.querySelector(".indexhtml")
         let articles = "";
         for (let i = 0; i < info.length; i++) {
-            articles += `<article>
+            section += `<section>
         <p class="name">Nombre:${tracks.data[i].title}>
          <a href="./detallegre.html?id=${tracks.data.link[i].id}">ir a detalle</a>
          <img class="image" src="${tracks.data.picture}" alt="">
-</article>`
+</section>`
+{/* <article class="item">
+                    <p>Old Town Road - Lil Nas X</p><br>
+                </article> */}
         }
         console.log(articles);
         section.innerHTML += articles
