da416b8220b258ce2e9c0a252fcb64c2fc6694f9
mrubaja@udesa.edu.ar <mrubaja@udesa.edu.arc:/Users/Usuario/Desktop/Programacin/proyecto>
2022-06-16 18:47:33 -0300

    songs

--- js/songs.js
+++ js/songs.js
@@ -15,11 +15,15 @@ fetch(urlSongs)
          let picture = document.querySelector('.foto');
          let artista = document.querySelector('.cancion')
          let disco = document.querySelector('.cancion2')
+    
          name.innerText = data.title;
          picture.src = data.album.cover;
          artista.innerText=data.artist.name;
          disco.innerText= data.album.title
-         
+         document.querySelector('.player').innerHTML=`<iframe title="deezer-widget" src="https://widget.deezer.com/widget/dark/track/${idSongs}" width="100%" height="300" frameborder="0" allowtransparency="true" allow="encrypted-media; clipboard-write"></iframe>`
+         document.querySelector('.button').innerHTML= `<button type="submit">Agregar a mi playlist</button>`
+         document.querySelector('.link').innerHTML=`<a href="./playlist.html">playlist</a>`
+    
          
        
     })
