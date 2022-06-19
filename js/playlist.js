c876c44dec89273caa5394ad8895ec5a3fbb4725
mrubaja@udesa.edu.ar <mrubaja@udesa.edu.arc:/Users/Usuario/Desktop/Programacin/proyecto>
2022-06-18 21:32:01 -0300

    agregar a favoritos

--- js/playlist.js
+++ js/playlist.js
@@ -1,73 +1,34 @@
-let queryString = location.search; //obtengo la qs
-let qsToObject = new URLSearchParams(queryString); //Un objeto literal basado en la qs
-let idSongs = qsToObject.get('id'); //obtengo el id del artista
+let recuperoStorage = localStorage.getItem("favoritos");
+recuperoStorageToArray = JSON.parse(recuperoStorage);
 
+//capturar la sección para completar con datos
+let section= document.querySelector(".favoritos")
+let contenidoSection = ""
 
-let urlSongs = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/tracks${idSongs}`;
-console.log(urlSongs);
-fetch(urlSongs)
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function (data) {
-        console.log(data);
-         let name = document.querySelector('.title-artist');
-         let picture = document.querySelector('.foto');
-         let artista = document.querySelector('.cancion')
-         let disco = document.querySelector('.cancion2')
+//pedir a la api los datos de todos los ids
+for(let i=0; i<recuperoStorageToArray.length; i++){
+    //fetch
+    let url = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${recuperoStorageToArray[i]}`
     
-         name.innerText = data.title;
-         picture.src = data.album.cover;
-         artista.innerText=data.artist.name;
-         disco.innerText= data.album.title
-         document.querySelector('.player').innerHTML=`<iframe title="deezer-widget" src="https://widget.deezer.com/widget/dark/track/${idSongs}" width="100%" height="300" frameborder="0" allowtransparency="true" allow="encrypted-media; clipboard-write"></iframe>`
-         document.querySelector('.button').innerHTML= `<button type="submit">Agregar a mi playlist</button>`
-         document.querySelector('.link').innerHTML=`<a href="./playlist.html">playlist</a>`
+    fetch(url)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data){
+            console.log(data);
+           //trabajo con los datos 
+            contenidoSection += `<article>
+                                    <p class="name">Nombre: ${data.title}</p>
+                                    <img src="${data.artist.picture}" alt="foto">
+                                     
+                            </article>`
 
-         let playlist =[];
-         let recuperoStorage = localStorage.getItem ("playlist");
-         if (recuperoStorage){
-             let playlistToArray = JSON.parse(recuperoStorage)
-             playlist = playlistToArray
-         }
+                            section.innerHTML = contenidoSection
 
-         
+        })
+        .catch(function(error){
+            console.log(error);
+        })
 
-         let link = document.querySelector('.button');
-         if(playlist.includes(idSongs)){
-             link.innerText = "sacar de favoritos"
-         }
-         if (playlist.includes(idSongs)){
-             let sacarSong = playlist.indexOf(idSongs)
-             playlist.splice(sacarSong,1);
-             link.innerText = "Agregar a favoritos"
-         }else{
-             favoritos.push(idSongs)
-             link.innerText = "Sacar de favoritos"
-         }
-         
-         let playlistToString = JSON.stringify(playlist);
-         localStorage.setItem('playlist',playlistToString)
-         
+}
 
-    
-         
-       console.log(localStorage.getItem("favoritos"));
-    })
-    .catch(function (error) {
-        console.log(error)
-    })
-
-
-
-
-
-
-
-
-
-
-
-
-
-let playlistRecuperada = JSON.parse(recuperoStorage);
--- js/songs.js
+++ js/songs.js
@@ -12,6 +12,7 @@ fetch(urlSongs)
     .then(function (data) {
         console.log(data);
          let picture = document.querySelector('.foto');
+         let name = document.querySelector('.title-artist');
          let artista = document.querySelector('.cancion')
          let disco = document.querySelector('.cancion2')
     
@@ -20,11 +21,54 @@ fetch(urlSongs)
          artista.innerText=data.artist.name;
          disco.innerText= data.album.title;
          document.querySelector('.player').innerHTML=`<iframe title="deezer-widget" src="https://widget.deezer.com/widget/dark/track/${idSongs}" width="100%" height="300" frameborder="0" allowtransparency="true" allow="encrypted-media; clipboard-write"></iframe>`
-         document.querySelector('.button').innerHTML= `<button type="submit">Agregar a mi playlist</button>`
+         document.querySelector('.play').innerHTML= `<button type="submit">Agregar a mi playlist</button>`
          document.querySelector('.link').innerHTML=`<a href="./playlist.html">playlist</a>`
     
          
-       
+         let favoritos = [];
+
+         //chequear si hay algo en favoritos
+         let recuperoStorage = localStorage.getItem("favoritos");
+ 
+         if(recuperoStorage){ //null o undefined => false //tiene una variable => true
+             let favoritosToArray = JSON.parse(recuperoStorage)
+             favoritos = favoritosToArray
+         }
+ 
+         let link= document.querySelector("button");
+ 
+         if(favoritos.includes(idSongs)){
+             //cambiarle el mensaje al usuario.
+             link.innerText = "Sacar de favoritos"
+         }
+ 
+         //Definir un evento para el link
+         link.addEventListener("click", function(evento){
+             //evitar default del link
+             evento.preventDefault()
+ 
+             if(favoritos.includes(idSongs)){
+                 //Sacar el id del array
+                 let gifASacar = favoritos.indexOf(idSong)
+                 favoritos.splice(gifASacar, 1);
+                 link.innerText="Agregar a favoritos"
+ 
+             } else {
+                 //Agregar un data al array
+                 favoritos.push(idSongs);
+                 link.innerText = "Sacar de favoritos"
+ 
+             }
+ 
+             //Agregar el array a localStorage. Antes hay que pasarlo a string
+             let gifFavoritosToString = JSON.stringify(favoritos);
+             localStorage.setItem('favoritos', gifFavoritosToString)
+ 
+ 
+             console.log(localStorage.getItem("favoritos"));
+ 
+         })
+ 
     })
     .catch(function (error) {
         console.log(error)
