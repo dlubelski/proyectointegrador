9a654b404b57dbe9e957a848474c6118da7d9966
serenagraciano <sgraciano@udesa.edu.ar>
2022-06-20 17:15:55 -0300

    formulario hecho

--- js/busqueda.js
+++ js/busqueda.js
@@ -5,26 +5,29 @@ console.log(qs);
 console.log(objeto);
 
 let busqueda =`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/artist/?q=${objeto}`;
+let listaArtistas = document.querySelector('.artistasSearch');
+listaArtistas.innerHTML = '<img class="cargando" src="./img/udemusicphotos/gif.gif">'
+
 fetch(busqueda)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
         console.log(data);
-
+        let titulo = document.querySelector('.titulosSearch')
         let artista = data.data
-        let listaArtistas = document.querySelector('.artistasSearch');
-        /* if (artista.length == 0) {
-            titulo.innerHTML = '¡La canción que quisiste buscar no existe!'
-            resultado.innerHTML = ''
+        if (artista.length == 0) {
             listaArtistas.innerHTML = '<h1>¡No se encontraron resultados!</h1>'
-        } */
+            return
+        } 
+    let resultados = ""
         for (let i = 0; i < 4; i++) {
-            listaArtistas.innerHTML +=
+            resultados +=
                 `<article class="artista">
     <img class="img" src="${artista[i].picture_big}" alt="${ artista[i].name }"> </a>
     <h3><a href="artists.html?id=${artista[i].id}">${ artista[i].name}</a>
     </h3></article>`
 }
+listaArtistas.innerHTML = resultados
 
 }) 
