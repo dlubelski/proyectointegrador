b3565c14ba85539e29ca6e9151d764563e1479cf
serenagraciano <sgraciano@udesa.edu.ar>
2022-06-23 12:46:22 -0300

    se superpusieron cosas

--- js/detallegre.js
+++ js/detallegre.js
@@ -1,11 +1,7 @@
 let queryString = location.search; //obtengo la qs
 let qsToObject = new URLSearchParams(queryString); //Un objeto literal basado en la qs
 let idUrl = qsToObject.get('id'); //obtengo el id del detalle del genero
-<<<<<<< HEAD
-let urlDetallegre = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/" + "idUrl" + "/artist"
-=======
-let urlDetallegre = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/${idUrl}`;
->>>>>>> f1c3dbe85c9a94757e177ed074c8e6932ec8fd14
+let urlDetallegre = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/" + idUrl
 
 fetch(urlDetallegre)
     .then(function (response) {
@@ -14,7 +10,8 @@ fetch(urlDetallegre)
     .then(function (data) {
         console.log(data);
         let info = data.data;
-        let article = document.querySelector(".detallegredata");
+        let section = document.querySelector(".detallegredata");
+        let article = "";
 
         for (let i = 0; i < 1; i++) {
             article += `<article class="regguetonartists"> 
